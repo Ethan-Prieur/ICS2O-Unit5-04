738addf0dc265fa05926002c2781b37c13120359
EthanPrieur <ethan.prieur@ocsbstudent.ca>
2021-01-21 15:53:42 +0000

    made it work

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-var base1 = document.getElementById("math1").value;
-var base2 = document.getElementById("math2").value;
-var height = document.getElementById("math3").value;
-function myFunction (base1, base2, height) {
-  var result = (base1 + base2) * height / 2;
-  document.getElementById("answer").innerHTML = result;
+document.getElementById('btn').addEventListener('click', myFunction)
+function myFunction () {
+  let base1 = document.getElementById("math1").value;
+  base1 = parseInt(base1)
+  let base2 = document.getElementById("math2").value;
+  base2 = parseInt(base2)
+  let height = document.getElementById("math3").value;
+  height = parseInt(height)
+  let answer = (base1 + base2) * height / 2
+  answer = parseInt(answer)
+  alert(answer)
 }
